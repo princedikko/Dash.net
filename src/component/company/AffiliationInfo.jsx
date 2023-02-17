9aa247bd184b1d7ee0b7a0d57b02c07c4d82a6ee
princedikko <princedikko@gmail.com>
2023-02-17 01:22:03 +0100

    send

--- src/component/company/AffiliationInfo.jsx
+++ src/component/company/AffiliationInfo.jsx
@@ -20,13 +20,29 @@ function AffiliationInfo() {
                     </div>
                     <div className="affiSteps">
                         <div className="affiStepOne">
+                        <figure>
+                               <img src={AffiImg} alt="dash.net" />
+                            </figure>
                             <div className="stepDiscription">
-                                <h5>Step 1: Sign Up and Get the Referral Link</h5>
+                                <h3>Step 1: <br /> Sign Up and Get the Referral Link</h3>
+                                <p>Create an affiliate account. Once you’ve signed up, you will have the access to your data overview dashboard as well as your EXCLUSIVE referral link.</p>
+                            </div>
+                            
+                            <div className="stepDiscription">
+                                <h3>Step 2: <br /> Spread the word</h3>
                                 <p>Create an affiliate account. Once you’ve signed up, you will have the access to your data overview dashboard as well as your EXCLUSIVE referral link.</p>
                             </div>
                             <figure>
                                <img src={AffiImg} alt="dash.net" />
                             </figure>
+                            <figure>
+                               <img src={AffiImg} alt="dash.net" />
+                            </figure>
+                            <div className="stepDiscription">
+                                <h3>Step 3: <br /> Get Paid</h3>
+                                <p>Create an affiliate account. Once you’ve signed up, you will have the access to your data overview dashboard as well as your EXCLUSIVE referral link.</p>
+                            </div>
+                            
                         </div>
                     </div>
 
