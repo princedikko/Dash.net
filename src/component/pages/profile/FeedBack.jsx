29ebe57fa3d35ebab7ecd1f186a16223c8a9bc53
princedikko <princedikko@gmail.com>
2023-02-19 12:12:53 +0100

    feedback

--- src/component/pages/profile/FeedBack.jsx
+++ src/component/pages/profile/FeedBack.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-
+import NavBar from "../../header/NavBar";
+import Logo from "../../pages/login/logImage/photo2.png"
+import { Link } from "react-router-dom";
 function FeedBack() {
-  return <section className="sectionFeedBack">this is all FeedBacks</section>;
+  return <section className="sectionFeedBack">
+    
+    <div className="feedbackHead">
+        <NavBar />
+      </div>
+      <div className="feedbackCont ">
+        <h2>Give us feedback</h2>
+        <div className="RpForm">
+          <form action="#" id="feedback">
+            
+            <label htmlFor="fullname">Full Name:</label>
+            <input type="text" placeholder="Full Name.." required/>
+            <label htmlFor="email">Email or Phone number </label>
+            <input type="text" name="Rpemail" id="" placeholder="Enter email or number" required/>
+            <label htmlFor="inquiry">State</label>
+            <select name="Rpinquire" id="inquiryRp" >
+              <option value=" "  disabled selected hidden required>select..</option>
+              <option value="caseOne" >Sokoto State</option>
+              <option value="caseOne" >Kaduna State</option>
+              <option value="caseOne" >Lagos State</option>
+              <option value="caseOne" >Kwara State</option>
+            </select>
+            <label htmlFor="helping">Your Message:</label>
+            <textarea name="Rpmessage" id="messaging" cols="30" rows="10" placeholder="I would like to say about..." required></textarea>
+            <input type="submit" value="Send message" />
+          </form>
+        </div>
+        </div>
+  </section>;
 }
 
 export default FeedBack;
